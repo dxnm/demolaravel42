43fe34443f3e89b66750d25d8e7de818017a0608
Administrator <Administrator@CUCON>
2015-10-29 11:07:57 +0700

    demo laravel 4.2 - CRUD - login

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>App Name - @yield('title')</title>
+        <title>@yield('title') - Demo Laravel 4.2</title>
         {{HTML::style('css/style.css') }}
     </head>
     <body>
